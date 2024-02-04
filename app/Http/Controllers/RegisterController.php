7b88af6b8323160b2fb68ec4b50f14236310bf14
Ghiyas111 <143326834+Ghiyas111@users.noreply.github.com>
2024-02-04 13:34:11 +0700

    saya menabhkan login
    menambahkan login dan register

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Session;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+use App\Mail\MailSend;
+
+class RegisterController extends Controller
+{
+    public function register()
+    {
+        return view('register');
+    }
+    
+    public function actionregister(Request $request)
+    {
+        $str = Str::random(100);
+
+        $user = User::create([
+            'email' => $request->email,
+            'name' => $request->username,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+            'verify_key' => $str
+        ]);
+
+        $details = [
+            'name' => $request->username,
+            'role' => $request->role,
+            'website' => 'www.ayongoding.com',
+            'datetime' => date('Y-m-d H:i:s'),
+            'url' => request()->getHttpHost().'/register/verify/'.$str
+        ];
+
+        Mail::to($request->email)->send(new MailSend($details));
+
+        Session::flash('message', 'Link verifikasi telah dikrim ke Email Anda. Silahkan Cek Email Anda untuk Mengaktifkan Akun');
+        return redirect('register');
+    }
+    
+    public function verify($verify_key)
+    {
+        $keyCheck = User::select('verify_key')
+                    ->where('verify_key', $verify_key)
+                    ->exists();
+        
+        if ($keyCheck) {
+            $user = User::where('verify_key', $verify_key)
+            ->update([
+                
+            ]);
+            
+            return "Verifikasi Berhasil. Akun Anda sudah aktif.";
+        }else{
+            return "Key tidak valid!";
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +11,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
+
+//REGISTER
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
+
+Route::get('register/verify/{verify_key}', [RegisterController::class, 'verify'])->name('verify');
