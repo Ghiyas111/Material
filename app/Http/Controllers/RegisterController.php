a76c33d21f7246f761939886f1a48882ec3432fe
aetaja <nepalam71@gmail.com>
2024-02-06 11:44:49 +0700

    Fixed problem
    credit : niell

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Session;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
-use App\Mail\MailSend;
 
 class RegisterController extends Controller
 {
@@ -16,48 +13,16 @@ class RegisterController extends Controller
     {
         return view('register');
     }
-    
+
     public function actionregister(Request $request)
     {
-        $str = Str::random(100);
-
         $user = User::create([
             'email' => $request->email,
             'name' => $request->username,
-            'password' => Hash::make($request->password),
-            'role' => $request->role,
-            'verify_key' => $str
+            'password' => Hash::make($request->password)
         ]);
 
-        $details = [
-            'name' => $request->username,
-            'role' => $request->role,
-            'website' => 'www.ayongoding.com',
-            'datetime' => date('Y-m-d H:i:s'),
-            'url' => request()->getHttpHost().'/register/verify/'.$str
-        ];
-
-        Mail::to($request->email)->send(new MailSend($details));
-
-        Session::flash('message', 'Link verifikasi telah dikrim ke Email Anda. Silahkan Cek Email Anda untuk Mengaktifkan Akun');
+        Session::flash('message', 'Register Berhasil. Akun Anda sudah Aktif silahkan Login menggunakan username dan password.');
         return redirect('register');
-    }
-    
-    public function verify($verify_key)
-    {
-        $keyCheck = User::select('verify_key')
-                    ->where('verify_key', $verify_key)
-                    ->exists();
-        
-        if ($keyCheck) {
-            $user = User::where('verify_key', $verify_key)
-            ->update([
-                
-            ]);
-            
-            return "Verifikasi Berhasil. Akun Anda sudah aktif.";
-        }else{
-            return "Key tidak valid!";
-        }
     }
 }
